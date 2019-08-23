a2f673e402efa9426f353358af72322f6fa5923a
ggoowlgns <30282681+ggoowlgns@users.noreply.github.com>
2019-08-23 23:25:52 +0900

    update 'update_bandwidth'
    added 'excepting handling' when "ffmpeg"is out (changing -maxrate)
    
    added : TimeoutError, URLError (when connection error with Nginx http)

--- test.py
+++ test.py
@@ -1,11 +1,20 @@
 #asdsansldasd
 import update_bandwidth
+import urllib.request
+
 if __name__ == "__main__":
     UB = update_bandwidth.UpdateBandwidth()
 
     while True:
-        UB.update_page()
-        in_bw, stream_meta = UB.get_data()
-        print(in_bw)
-        print( stream_meta)
-        print()
+        try:
+            UB.update_page()
+            in_bw, stream_meta = UB.get_data()
+            print(in_bw)
+            print( stream_meta)
+            print()
+        except AttributeError:
+            print("로딩중 입니다.")
+        except TimeoutError:
+            print("연결이 끊겼습니다. 잠시만 기다려주세요")
+        except urllib.error.URLError:
+            print("연결이 끊겼습니다. 잠시만 기다려주세요22")
