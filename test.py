313385cb97e6d1220a9ad47dd17b612feb4b8a77
ggoowlgns <30282681+ggoowlgns@users.noreply.github.com>
2019-08-23 19:29:32 +0900

    update 'update_bandwidth' made UpdateBandwidth()
    Usage : ref  - test.py

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
 #asdsansldasd
+import update_bandwidth
+if __name__ == "__main__":
+    UB = update_bandwidth.UpdateBandwidth()
+    UB.update_page()
+    in_bw , stream_meta = UB.get_data()
+    print(in_bw)
+    print( stream_meta)
--- update_bandwidth.py
+++ update_bandwidth.py
@@ -2,32 +2,48 @@ import xml.etree.ElementTree as ET
 import urllib.request
 from bitmath import *
 
-
-def format_bytes(size):
-    # 2**10 = 1024
-    power = 2**10
-    n = 0
-    power_labels = {0 : '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
-    while size > power:
-        size /= power
-        n += 1
-    return size, power_labels[n]+'bytes'
+class UpdateBandwidth:
+    def __init__(self):
+        self.in_bw = 0
+        self.stream_meta = {}
 
 
-webPage_xml = urllib.request.urlopen("http://61.253.199.32:81/stat.xml")
-webPage_xml = webPage_xml.read().decode("utf-8")
+    def format_bytes(self, size):
+        # 2**10 = 1024
+        power = 2**10
+        n = 0
+        power_labels = {0 : '', 1: 'kilo', 2: 'mega', 3: 'giga', 4: 'tera'}
+        while size > power:
+            size /= power
+            n += 1
+        return size, power_labels[n]+'bytes'
 
-# print(webPage_xml)
+    def update_page(self):
+        webPage_xml = urllib.request.urlopen("http://61.253.199.32:81/stat.xml")
+        webPage_xml = webPage_xml.read().decode("utf-8")
 
-root = ET.fromstring(webPage_xml)
+        # print(webPage_xml)
 
-# print(root.tag, root.attrib)
+        root = ET.fromstring(webPage_xml)
 
-bandwidth_per_sec = int(root.findtext("bw_in"))
-BW_bits = format_bytes(bandwidth_per_sec)
-print(BW_bits)
+        # print(root.tag, root.attrib)
 
+        bandwidth_per_sec = int(root.findtext("bw_in"))
+        # stream_live = root.find("server")
+        stream_list = root.find("server").find("application").find("live").findall("stream")
+        stream_name_resolution = {}
+        for stream in stream_list:
+            stream_name_resolution[stream.findtext("name")] = [stream.find("meta").find("video").findtext("width"),stream.find("meta").find("video").findtext("height")]
+        BW_bits = self.format_bytes(bandwidth_per_sec)
 
+        self.in_bw = BW_bits
+        self.stream_meta = stream_name_resolution
+        # print(stream_name_resolution)
+        # print(BW_bits)
+
+    def get_data(self):
+
+        return self.in_bw, self.stream_meta
 
 
 # R E F
